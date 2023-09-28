fd67a26cf432dfeb970773c07b5a3dd471630b86
Dee <deepakdilse@gmail.com>
2023-09-28 00:04:51 -0700

    update script to generate password

--- script.js
+++ script.js
@@ -1,39 +1,121 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//declare all variables to store user prompt/inputs
-var passwordLength;
-var includeUpperCase;
-var includeLowerCase;
-var includeNumbers;
-var includeSpecialChars;
+//declare all variables to store user entries 
+var passwordLength; //length of password
+var includeUpperCase; //include upper case charcters
+var includeLowerCase; //include lower case characters
+var includeNumbers; //include numbers
+var includeSpecialChars; //include special characters
 
-var passwordCharacteroptions;
-var passwordArray = [];
+//store all possible characters in this array.
+var passwordCharacterOptions;
+//stores the password characters
+var passwordArray;
 
 
-// define all the sub parts of the pssword.  upper case. lower case, special characters, numbers
+// define all the valid characters of the password.  
+// upper case, lower case, special character and numbers.
 const upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowerCaseCharacters = upperCaseCharacters.map(char => char.toLowerCase());
-
-// console.log("lowerCaseCharacters ", lowerCaseCharacters);
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// console.log("specialCharacters ", specialCharacters);
 
-// line 4
-// declare method generate password
+// create a function to get length of password from user 
+function getPasswordLength() {
+  passwordLength = prompt("Please specify Password length. \nMust be between 8 and 128");
+  //convert passwordLength to number
+  passwordLength = parseInt(passwordLength);
+}
 
+
+// alert user and restart generatePassword() function , if password length isnt between 8 and 128. 
+function validatePasswordLength() {
+  // check if password length is not between 8 and 128
+  if (!(passwordLength && passwordLength >= 8 && passwordLength <=128)) {
+    window.alert("Invalid password length. \nPassword length should be between 8 and 128 !! "); //alert the user for invalid input
+    generatePassword(); // restart the process to generare password
+  }
+}
+
+// save password criteria inputs from user into their variables
+function getPasswordCriteria() {
+  includeUpperCase = window.confirm("Include upper case characters in the password?");
+  includeLowerCase = window.confirm("Include lower case characters in the password?");
+  includeNumbers = window.confirm("Include numbers in the password?");
+  includeSpecialChars = window.confirm("Include special characters in the password?");
+}
+
+
+//function to check if user has selected AT LEAST 1 of the types of characters to be included in password
+function validatePasswordCriteria() {
+  //if they are all false, its not a valid case !!
+  if(!(includeUpperCase || includeLowerCase || includeNumbers || includeSpecialChars)) {
+    window.alert("Invalid Password criteria. \nSelect at least one of the types of characters to be included in the password.");
+    generatePassword();
+  }
+}
+
+//reset all variables that store user input about password length and password criteria
+function resetUserInputVariables() {
+  passwordLength = undefined;
+  includeUpperCase = undefined;
+  includeLowerCase = undefined;
+  includeNumbers = undefined;
+  includeSpecialChars = undefined;
+}
+
+// Call functions here to get user input, validating inputs, generating password and returning password.
 function generatePassword() {
+  resetUserInputVariables()  //reset all user inputs
+  getPasswordLength();
+  validatePasswordLength();
+  getPasswordCriteria();
+  validatePasswordCriteria();
+  combineValidChars();
+  getPasswordCharacters();
+  // avoid returning null,undefined and empty strings.
+  // join the array with '' params to convert it to a string.
+  return passwordArray.length > 0 ? passwordArray.join(''): ''; // if password array is not empty, create a string from the array and return it. else return '' string
+}
+
+
+//generate random index number  using length of the array that has all the possible options to be included in the password.
+function randomIndexGenerator() {
+  //Math.random returns a number between 0 and 1.
+  //Math.floor rounds a number DOWN to the nearest integer
+  return Math.floor(Math.random() * passwordCharacterOptions.length); //returns a number that is  >= 0, and  <= length of array containing all possible characters for password.
+}
+
+// generate an array with all possible characters that can be in the password based on user provided criteria
+function combineValidChars() {
+  //reset the passwordCharacterOptions 
+  passwordCharacterOptions = [];
+  //use spread operator to copy the characters, to be included in password into the passwordCharacterOptions array.
+  if(includeUpperCase) {
+    passwordCharacterOptions = [...upperCaseCharacters]; //add upper case characters into passwordCharacterOptions
+  }
+  if(includeLowerCase) {
+    passwordCharacterOptions = [...passwordCharacterOptions, ...lowerCaseCharacters]; //add lower case characters
+  }
+  if(includeNumbers) {
+    passwordCharacterOptions = [...passwordCharacterOptions, ...numbers]; //add numbers 
+  }
+  if(includeSpecialChars) {
+    passwordCharacterOptions = [...passwordCharacterOptions, ...specialCharacters]; //add special characters
+  }
+}
+
+// pick random characters from passwordCharacterOptions to be used in password.
+// in each loop run, push one random character from the passwordCharacterOptions into the password array.
+function getPasswordCharacters() {
+  // Reset the passwordArray
   passwordArray = [];
-  //this.printUserInputs();
-  getUserInputs();
-  printUserInputs();
-  console.log("Password is : ", passwordArray, passwordArray.join());
-  return passwordArray.join('');
-  // passwordLength = prompt("Please specify the length of the password, between 8 to 32 characters");
-  // alert(`Password length is: ${passwordLength}`);
-  // console.log("Random index generated !!",Math.floor(Math.random() * passwordLength));
+  //loop runs for as many times as the password length
+  for (i = 0; i < passwordLength; i++) {
+    //push 1 randomn character from passwordCharacterOptions into passwordArray
+    passwordArray.push(passwordCharacterOptions[randomIndexGenerator()]);
+  }
 }
 
 // Write password to the #password input
@@ -47,158 +129,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// create a function to get user inputs 
-function getUserInputs() {
-  passwordLength = prompt("Please specify a Password length. \nBetween 8 to 32");
-  this.validatePasswordLength();
-
-}
-
-// get password criiteria inputs
-function getPasswordCriteria() {
-  includeUpperCase = window.confirm("Include upper case characters ?");
-  includeLowerCase = window.confirm("Include lower case characters ?");
-  includeNumbers = window.confirm("Include numbers ?");
-  includeSpecialChars = window.confirm("Include special characters ?");
-  this.validatePasswordCriteria();
-
-}
-
-
-//create a function to check if user has selected AT LEAST 1 of the types of characters
-function validatePasswordCriteria() {
-  //if they are all false, its not a valid case !!
-  if((includeUpperCase || includeLowerCase || includeNumbers || includeSpecialChars)) {
-    
-    // this.call method to create super set array of password !
-    this.combineValidChars();
-    //create password !
-    this.getPasswordCharacters();
-  } else {
-    window.alert("Invalid Password criteria. \nSelect at least one of the types of characters to be included in the password. \nTry again !! ");
-  }
-}
-
-//generate random index using passwordLength
-function randomIndexGenerator() {
-  var randomIndex = Math.floor(Math.random() * this.passwordCharacteroptions.length);
-  console.log("randomIndex ", randomIndex);
-  return randomIndex;
-
-}
-
-// generate password from critera
-function combineValidChars() {
-  passwordCharacteroptions = [];
-  if(includeUpperCase) {
-    passwordCharacteroptions = [...upperCaseCharacters];
-  }
-  if(includeLowerCase) {
-    passwordCharacteroptions = [...passwordCharacteroptions, ...lowerCaseCharacters];
-  }
-  if(includeNumbers) {
-    passwordCharacteroptions = [...passwordCharacteroptions, ...numbers];
-  }
-  if(includeSpecialChars) {
-    passwordCharacteroptions = [...passwordCharacteroptions, ...specialCharacters];
-  }
-  console.log("ppasswordoptions: ", passwordCharacteroptions)
-}
-
-function getPasswordCharacters(){
-  this.passwordArray = [];
-  for(i=0; i< this.passwordLength; i++) {
-    this.passwordArray.push(this.passwordCharacteroptions[this.randomIndexGenerator()]);
-  }
-  console.log("generated password: ", this.passwordArray, this.passwordArray.join(''));
-}
-
-// // create password
-// function createPassword() {
-//   this.passwordArray = [];
-//   for(i=0; i<passwordLength; i++) {
-//     console.log("loop ", i);
-//     this.passwordArray.push(this.passwordCharacteroptions[this.randomIndexGenerator()]);
-//     console.log("password ", passwordArray);
-//   }
-// }
-
-// function abc() {
-//   this.passwordArray = [];
-  
-// }
-
-//function to create new pass 
-// function abc() {
-//   this.password = [];
-//   for(i=0; i<passwordLength; i++) {
-//     console.log("loop ", i);
-//     this.password.push(this.passwordCharacteroptions[this.randomIndexGenerator()]);
-//     console.log("password ", password);
-// }
-
-// function to print all user answers !!
-function printUserInputs() {
-  console.log("passwordLength: ",passwordLength);
-  console.log("include upper: ", includeUpperCase);
-  console.log("inclde lower: ", includeLowerCase);
-  console.log("include number: ", includeNumbers);
-  console.log("include special chars: ", includeSpecialChars);
-}
-
-// check if password length is between 8 and 32
-function validatePasswordLength() {
-  if ((passwordLength && passwordLength >= 8 && passwordLength <=32)) {
-    //get password crietria !!
-    this.getPasswordCriteria();
-  } else {
-    window.alert("Invalid password length. \nTry again !! ");
-  }
-
-}
-
-// prompt user to input how many characters
-// if input is between 8 and 128, we accept else, invalid response.
-// Confirm whether user wants upper case characters
-// Confirm whether user wants lower case characters
-// Confirm whether user wants numbers
-// Confirm whether user wants special characters
-
-// User needs to select atleast  one of the above conditions !!
-// IF user doesnt select any of the above rules, alert : At least one should be selected.
-
-
-// store these responses in global vars
-
-//store all upper case letters in a variable //
-//store all lower case letters in a variable //cmd + shift + down array to copy last line and paste it to next !
-//store all numbers case letters in a variable
-//store all special chars in a variable
-//store all upper case letters in a variable
-
-
-//startr creating password !!
-//create a string by combining all the options: upper case, lower case, numbers, special chars !!
-
-
-
-
-
-
-
-//declare an empty array.
-// push all the password into this array,- master password set !!
-// create a string from this array !! --some method exists !
-
-
-
-
-
-//how to build passwords !!
-
-//random numbers 
-// Math.floor();   //floor(0, 1) => 0;
-// Math.random();  //
-
-//Math.floor(Math.random() * 10); //("number of chars in paswd"));
