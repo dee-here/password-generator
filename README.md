e45115e4c00bfba413e6d7768dff97715cafe30f
Dee <deepakdilse@gmail.com>
2023-09-28 21:10:26 -0700

    Update readme, format js file

--- README.md
+++ README.md
@@ -1,92 +1,90 @@
-# 03 JavaScript: Password Generator
+# Password Generator
+An application that generates random passwords based on provided criteria.
 
-## Your Task
 
-This week’s challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+## Technology Used 
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+| Technology Used         | Resource URL           | 
+| ------------- |-------------| 
+| JavaScript    | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) | 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     |    
 
-## User Story
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+## Description
+[Password Generator](https://dee-here.github.io/password-generator/)
 
-## Acceptance Criteria
+This project was to modify starter code to create an application that generates random passwords based on criteria provided to the application.
+Starter code included a JavaScript file where I could write code to get the desired output.
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+Logic to generate password:
+* Prompt the user for a password length.
+* Validate password length. It should be between 8 and 128.
+* If password length is valid continue to get password criteria from user, else alert user to try again and stop.
+* Get password criteria from user. Password criteria specifies if password should include uppercase, lower case, special characters and/or numbers.
+* Validate password criteria entered by user. User should include atleast one type of characters, else alert user to try again and stop.
+* If password criteria is valid, generate an array with all the characters that the user wants to include in the password.
+* Using a random index generator, copy the number of characters needed in the password from the array created in the previous step to an array called passwordArray.
+* Convert this passwordArray into a string and display it in the text box.
 
-## Mock-Up
 
-The following image shows the web application's appearance and functionality:
+## Usage
+Click on the red "Generate Password" button to get started.
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-challenge-demo.png)
+Enter a password length when prompted. Password length needs to be between 8 and 128.
 
-## Grading Requirements
+Follow the prompts to provide password criteria. If valid criteria is entered, the applciation generates a new password and displays it in the text box.
 
-This challenge is graded based on the following criteria: 
+Click on the red "Generate Password" to try again.
 
-### Technical Acceptance Criteria: 40%
 
-* Satisfies all of the preceding acceptance criteria plus the following:
 
-  * The challenge should not produce any errors in the console when you inspect it using Chrome DevTools.
+## Learning points
+Here are a few things that I learnt while working on this project.
 
-### Deployment: 32%
+JavaScript Alert box, Confirm box and Prompt can be used to display pop up dialogs and accept user inputs.
+Array map function and arrow functions are very useful to modify data.
+Use \ to escape special characters in JavaScript.
 
-* Application deployed at live URL.
+Math.random returns a random number between 0 and upto 1. 
+Math.floor rounds down and returns the largest integer less than or equal to a given number. 
 
-* Application loads with no errors.
+Spread operator is very useful to copy arrays.
 
-* Application GitHub URL submitted.
+## Screen Recording
+![password generator gif](./Assets/password-generator.gif)
 
-* GitHub repository that contains application code.
 
-### Application Quality: 15%
+## Author Info
 
-* Application user experience is intuitive and easy to navigate.
+Deepak Sinha
 
-* Application user interface style is clean and polished.
+* [Portfolio](https://dee-here.github.io/portfolio/)
+* [Github](https://github.com/dee-here)
 
-* Application resembles the mock-up functionality provided in the challenge instructions.
 
-### Repository Quality: 13%
 
-* Repository has a unique name.
+## License
 
-* Repository follows best practices for file structure and naming conventions.
+MIT License
 
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+Copyright (c) [2023] [deepak sinha]
 
-* Repository contains multiple descriptive commit messages.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -13,8 +13,9 @@ var passwordCharacterOptions;
 // stores the password as a character array
 var passwordArray;
 
-// define all the valid characters of the password and store them in separate arrays.
-// upper case, lower case, special character and numbers.
+// define all the valid characters of the password and store them in separate arrays. upper case, lower case, special character and numbers.
+
+//array to store all upper case characters
 const upperCaseCharacters = [
   "A",
   "B",
@@ -44,18 +45,20 @@ const upperCaseCharacters = [
   "Z",
 ];
 
-// convert upper case character array to lower case.
+// array to store all lower case characters
+// use upper case character array to create lower case character array.
 const lowerCaseCharacters = upperCaseCharacters.map((char) =>
   char.toLowerCase()
 );
 
+// special characters array
 // split a string of all special characters into an array using ""
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
 // numerical characters
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// function to get length of password from user
+// get length of password from user from prompt dialog
 function getPasswordLength() {
   passwordLength = prompt(
     "Please specify Password length. \nMust be a number between 8 and 128"
@@ -64,7 +67,7 @@ function getPasswordLength() {
   passwordLength = parseInt(passwordLength);
 }
 
-// check password length is  between 8 and 128
+// check if password length is  between 8 and 128
 function isValidPasswordLength() {
   return (passwordLength >= 8 && passwordLength <= 128);
 }
@@ -81,18 +84,12 @@ function validatePasswordLength() {
   }
 }
 
-// save password criteria inputs from user into their variables  ok: true , cancel: false
+// save password criteria inputs from user into their variables
 function getPasswordCriteria() {
-  includeUpperCase = window.confirm(
-    "Include upper case characters in the password?"
-  );
-  includeLowerCase = window.confirm(
-    "Include lower case characters in the password?"
-  );
+  includeUpperCase = window.confirm("Include upper case characters in the password?");
+  includeLowerCase = window.confirm("Include lower case characters in the password?");
   includeNumbers = window.confirm("Include numbers in the password?");
-  includeSpecialChars = window.confirm(
-    "Include special characters in the password?"
-  );
+  includeSpecialChars = window.confirm("Include special characters in the password?");
 }
 
 //function to check if user included at least one type of characters
@@ -109,16 +106,14 @@ function someCriteriaSelected() {
 // function to validate Password Criteria
 function validatePasswordCriteria() {
   //if no crietria is selecetd, its invalid input
-  if (someCriteriaSelected) {
+  if (someCriteriaSelected()) {
     return true;
   } else {
     //alert the user for invalid password criteria
     window.alert(
       "Invalid Password criteria. \nSelect at least one of the types of characters to be included in the password."
     );
-    //restart the function to generate password.
-    //generatePassword();
-    return false;
+    return false; 
   }
 }
 
@@ -131,11 +126,52 @@ function resetUserInputVariables() {
   includeSpecialChars = undefined;
 }
 
-// this function here calls all other functions:  get user input, validating inputs, generating password and returning password.
-function generatePassword() {
-  resetUserInputVariables(); //reset all user inputs
-  getPasswordLength(); //get passsword length
+//generate random index number.
+function randomIndexGenerator() {
+  //Math.random returns a number between 0 and 1.
+  //Math.floor rounds a number DOWN to the nearest integer
+  return Math.floor(Math.random() * passwordCharacterOptions.length); //returns a positive integer that is  >= 0, and  < length of array containing all possible characters for password.
+}
 
+// generate an array with all possible characters that can be in the password based on user criteria
+function combineValidChars() {
+  //reset the passwordCharacterOptions to prevent stale data
+  passwordCharacterOptions = [];
+  //use spread operator to copy the characters, to be included in password into the passwordCharacterOptions array.
+  if (includeUpperCase) {
+    //add upper case characters into passwordCharacterOptions
+    passwordCharacterOptions = [...upperCaseCharacters];
+  }
+  if (includeLowerCase) {
+    //add lower case characters
+    passwordCharacterOptions = [...passwordCharacterOptions, ...lowerCaseCharacters];
+  }
+  if (includeNumbers) {
+    //add numbers
+    passwordCharacterOptions = [...passwordCharacterOptions, ...numbers];
+  }
+  if (includeSpecialChars) {
+    //add special characters
+    passwordCharacterOptions = [...passwordCharacterOptions, ...specialCharacters];
+  }
+}
+
+// pick characters from passwordCharacterOptions to be used in password.
+function getPasswordCharacters() {
+  // reset the passwordArray
+  passwordArray = [];
+  // loop runs for as many times as the password length
+  // in each loop run, push one random character from the passwordCharacterOptions into the password array.
+  for (i = 0; i < passwordLength; i++) {
+    //push 1 character from the passwordCharacterOptions array at the random index created by randomIndexGenerator()  into the passwordArray on each loop
+    passwordArray.push(passwordCharacterOptions[randomIndexGenerator()]);
+  }
+}
+
+// main function that calls all other functions:  get user input, validating inputs, generating password and returning password.
+function generatePassword() {
+  resetUserInputVariables(); //reset all user inputs fromstale data
+  getPasswordLength(); //get passsword length
   // continue to get password criteria if password length is valid
   if (validatePasswordLength()) {
     getPasswordCriteria(); // get password criteria
@@ -146,53 +182,6 @@ function generatePassword() {
       // join the array with '' params to convert it to a string.
       return passwordArray.join(""); // create a string from the array and return it for display in text box
     }
-  }
-}
-
-//generate random index number  using length of the array that has all the possible options to be included in the password.
-function randomIndexGenerator() {
-  //Math.random returns a number between 0 and 1.
-  //Math.floor rounds a number DOWN to the nearest integer
-  return Math.floor(Math.random() * passwordCharacterOptions.length); //returns a number that is  >= 0, and  <= length of array containing all possible characters for password.
-}
-
-// generate an array with all possible characters that can be in the password based on user criteria
-function combineValidChars() {
-  //reset the passwordCharacterOptions to prevent stale data
-  passwordCharacterOptions = [];
-  //use spread operator to copy the characters, to be included in password into the passwordCharacterOptions array.
-  if (includeUpperCase) {
-    passwordCharacterOptions = [...upperCaseCharacters]; //add upper case characters into passwordCharacterOptions
-  }
-  if (includeLowerCase) {
-    //add lower case characters
-    passwordCharacterOptions = [
-      ...passwordCharacterOptions,
-      ...lowerCaseCharacters,
-    ];
-  }
-  if (includeNumbers) {
-    //add numbers
-    passwordCharacterOptions = [...passwordCharacterOptions, ...numbers];
-  }
-  if (includeSpecialChars) {
-    //add special characters
-    passwordCharacterOptions = [
-      ...passwordCharacterOptions,
-      ...specialCharacters,
-    ];
-  }
-}
-
-// pick characters from passwordCharacterOptions to be used in password.
-// in each loop run, push one random character from the passwordCharacterOptions into the password array.
-function getPasswordCharacters() {
-  // reset the passwordArray
-  passwordArray = [];
-  // loop runs for as many times as the password length
-  for (i = 0; i < passwordLength; i++) {
-    //push 1 character from the passwordCharacterOptions array at the random index created by randomIndexGenerator()  into the passwordArray on each loop
-    passwordArray.push(passwordCharacterOptions[randomIndexGenerator()]);
   }
 }
 
